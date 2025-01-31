76353b5103e6ec2b263fd1d278ffcf9161e18e80
Vignesh Nukala <125763653+VigneshNukala@users.noreply.github.com>
2025-01-31 04:05:05 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Verify from "./components/Verify";
 function App() {
   return (
     <Routes>
-      <Route path="/register" element={<Register />} />
+      <Route path="/" element={<Register />} />
       <Route path="/verify" element={<Verify />} />
       <Route path="/organization" element={<OrganizationSetup />} />
       <Route path="/integration" element={<Integration />} />
